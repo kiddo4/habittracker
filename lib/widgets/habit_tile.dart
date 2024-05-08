56475022bc0f87523d69cbdd9a782edb2d3ed9fd
Olanrewaju taiwo <107349861+kiddo4@users.noreply.github.com>
2024-05-09 00:51:30 +0100

    refactor: Update HabitTile widget to use padding and fix syntax error
    The code changes in `habit_tile.dart` update the `HabitTile` widget to use the `Padding` widget for consistent spacing. Additionally, a syntax error in the code has been fixed.

--- lib/widgets/habit_tile.dart
+++ lib/widgets/habit_tile.dart
@@ -17,48 +17,51 @@ class HabitTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Slidable(
-      endActionPane: ActionPane(
-        motion: const StretchMotion(), 
-        children: [
-          SlidableAction(
-            onPressed: editHabit,
-            icon: Icons.edit,
-            backgroundColor: Colors.grey.shade800,
-            borderRadius: BorderRadius.circular(10),
-
-            ),
-
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),,
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: const StretchMotion(), 
+          children: [
             SlidableAction(
-          onPressed: deleteHabit,
-          icon: Icons.delete,
-          backgroundColor:  Colors.red,
-          borderRadius: BorderRadius.circular(10),
-        )
-        ]),
-      child: GestureDetector(
-        onTap: () {
-          if (onChanged != null) {
-            onChanged!(!isCompleted);
-          }
-          
-        },
-        child: Container(
-          decoration: BoxDecoration(
-            color: isCompleted
-                ? Colors.green
-                : Theme.of(context).colorScheme.secondary,
+              onPressed: editHabit,
+              icon: Icons.edit,
+              backgroundColor: Colors.grey.shade800,
+              borderRadius: BorderRadius.circular(10),
+      
+              ),
+      
+              SlidableAction(
+            onPressed: deleteHabit,
+            icon: Icons.delete,
+            backgroundColor:  Colors.red,
             borderRadius: BorderRadius.circular(10),
+          )
+          ]),
+        child: GestureDetector(
+          onTap: () {
+            if (onChanged != null) {
+              onChanged!(!isCompleted);
+            }
+            
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              color: isCompleted
+                  ? Colors.green
+                  : Theme.of(context).colorScheme.secondary,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            padding: const EdgeInsets.all(8),
+            
+            child: ListTile(
+                title: Text(text),
+                leading: Checkbox(
+                  activeColor: Colors.green,
+                  value: isCompleted,
+                  onChanged: onChanged,
+                )),
           ),
-          padding: const EdgeInsets.all(8),
-          margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-          child: ListTile(
-              title: Text(text),
-              leading: Checkbox(
-                activeColor: Colors.green,
-                value: isCompleted,
-                onChanged: onChanged,
-              )),
         ),
       ),
     );
